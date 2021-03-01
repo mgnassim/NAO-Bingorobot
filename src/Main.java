ced8bfeecbd0a5acefeaf5d3c33a5a1b57239bd7
J.P.L. Brouwer <Joris.brouwer2@hva.nl>
2021-03-01 09:32:52 +0100

    basis spraakherkennigsmodule

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
         NAO nao = new NAO();
         nao.verbind("localhost", 9559);
+
+        List <String> woordenlijst = new ArrayList<>();
+        woordenlijst.add("Paard");
+        woordenlijst.add("Koe");
+        woordenlijst.add("melk");
+        woordenlijst.add("pizza");
+        woordenlijst.add("vogel");
+
+        nao.luisteren(woordenlijst);
+
     }
 }
