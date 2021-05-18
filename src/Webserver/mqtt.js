3bc89442fe98cebefcb38b53d916df8ed54d0a57
bilalma@ad.hva.nl <bilalma@ad.hva.nl>
2021-05-18 13:16:20 +0200

    website

--- src/Webserver/mqtt.js
+++ src/Webserver/mqtt.js
@@ -1,27 +1,50 @@
 var mqtt;
-var reconnectTimeout = 2000;
-var host = "mqtt.hva-robots.nl"; // change this
+const reconnectTimeout = 2000;
+var host = "mqtt.hva-robots.nl";
 var port = 443;
 var username = "bilalma";
 var password = "lo7ooKsNuabwdwvL2exq";
 
 function onConnect() {
-    // Once a connection has been made, make a sub and send a message.
     console.log("Connected");
-    message = new Paho.MQTT.Message("Dit is een test voor de bingo"); // bericht
-    message.destinationName = "bilalma/robot/bingo"; // topic
+    message = new Paho.MQTT.Message("Dit is een test voor de bingo");
+    message.destinationName = "bilalma/robot/bingo";
     mqtt.send(message);
 }
 
 function MQTTconnect() {
-    console.log("connecting to "+ host + "" + port);
+    console.log("connecting to " + host + "" + port);
     mqtt = new Paho.MQTT.Client(host, port, "clientjs");
     var options = {
-      useSSL: true,
-      timeout: 3,
+        useSSL: true,
+        timeout: 3,
         userName: username,
         password: password,
-      onSuccess: onConnect,
+        onSuccess: onConnect,
+    };
+
+    mqtt.connect(options); // connect
+}
+
+var sum = document.getElementById('searchTerm').value;
+
+function onConnect2() {
+    console.log("Connected");
+    message2 = new Paho.MQTT.Message(document.getElementById('searchTerm').value);
+    message2.destinationName = "bilalma/robot/bingo2";
+    console.log(message2);
+    mqtt.send(message2);
+}
+
+function MQTTconnect2() {
+    console.log("connecting to " + host + "" + port);
+    mqtt = new Paho.MQTT.Client(host, port, "clientjs");
+    var options = {
+        useSSL: true,
+        timeout: 3,
+        userName: username,
+        password: password,
+        onSuccess: onConnect2,
     };
 
     mqtt.connect(options); // connect
